80bc9260f641007c0157ee94ee6a5d419e4b72ea
iki1uc <entertrainer@web.de>
2026-09-03 21:40:11 +0200

    Refactor MassHWTranswarp to use pipeline function

--- rad.js
+++ rad.js
@@ -1,5 +1,10 @@
-MassHWTranswarp.set(
-    qi,        // Masse
-    iqq,       // Geschwindigkeit
-    TimeHW.delta
-);
+pipeline(axis) {
+  Pipeline9.useA81({
+    pipe9: axis.pipe9,
+    a81: this.a81,
+    nano: this.nano(),
+    rein: this.rein(),
+    impuls: this.impuls(),
+    zahnrad: this.zahnrad   // ← GENAU HIER REIN
+  });
+}
