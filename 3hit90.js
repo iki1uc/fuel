018431e86fbb047795f975c2050d1adb4f2955fd
iki1uc <entertrainer@web.de>
2026-09-03 23:10:48 +0200

    Update 3hit90.js

--- 3hit90.js
+++ 3hit90.js
@@ -1,1 +1,5 @@
+this.axis = ["d","e","i","n","o","r","s","u","w"][this.stage % 81];
+POTENZEN = [3, 9, 27, 81, 243, 729];
+this.axis = ["d","e","i","n","o","r","s","u","w"][this.stage % 3];
+POTENZEN = [3, 9, 27, 81, 243, 729];
 this.axis = ["d","e","i","n","o","r","s","u","w"][this.stage % 9];
