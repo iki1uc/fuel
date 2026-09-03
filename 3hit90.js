f0d9e9c4a9ebade28d05a56fb53bef47dba3665d
iki1uc <entertrainer@web.de>
2026-09-03 23:12:38 +0200

    Fix axis calculation based on stage value

--- 3hit90.js
+++ 3hit90.js
@@ -3,3 +3,4 @@ POTENZEN = [3, 9, 27, 81, 243, 729];
 this.axis = ["d","e","i","n","o","r","s","u","w"][this.stage % 3];
 POTENZEN = [3, 9, 27, 81, 243, 729];
 this.axis = ["d","e","i","n","o","r","s","u","w"][this.stage % 9];
+ 
