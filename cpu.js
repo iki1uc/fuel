92c46583ca782b3df6fe50fbf8a4d8d81da99d9a
iki1uc <entertrainer@web.de>
2026-09-03 21:45:00 +0200

    Update cpu.js

--- cpu.js
+++ cpu.js
@@ -1,4 +1,32 @@
 Verfahren.useA81 = function(axis){
-    this.stage = axis.matrix[0][0] * A81_AXIS.cplus;
+
+    // 1. Energetische Impulse aus axis.matrix
+    let energySum = 0;
+    axis.matrix.forEach(row => {
+        row.forEach(iqq => {
+            MassHWTranswarp.set(iqq, iqq, TimeHW.delta);
+            const e = MassHWTranswarp.compute();
+            energySum += e;
+        });
+    });
+
+    // 2. Mechanischer Impuls (Zahnrad)
+    const mech = state.zahnrad;
+
+    // 3. Innen-Lehre (A81)
+    const innen = state.a81;
+
+    // 4. Ableitungen
+    const nano = innen / 9;
+    const rein = Math.sqrt(innen);
+
+    // 5. BALANCED Stage (XYX-Zentrum)
+    this.stage = (
+        energySum * 0.33 +   // Energie
+        mech       * 0.33 +   // Mechanik
+        nano       * 0.17 +   // Ableitung 1
+        rein       * 0.17     // Ableitung 2
+    );
+
     return this.run();
 };
